874b66a811756781dfe2470348d937fb03f88bc6
daniel <dcordonl@esliceu.net>
2026-01-15 16:58:44 +0100

    pàgina home

---
+++ wp-data/wp-content/themes/infobasic/front-page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenidos a Motosseras web</title>
+</head>
+<body>
+    <h1>Bienvenidos a Motosseras web</h1>
+    <p>Este es el tema Infobasic desarrollado por Dleon.</p>
+    <a href="/blog/">Ir al Blog</a>
+</body>
+</html>

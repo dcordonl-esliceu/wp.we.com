d7df0a5c7c919c5113cf8b1c854a35b7dbc57a5a
daniel <dcordonl@esliceu.net>
2026-01-22 17:01:38 +0100

    functions 1.0

--- wp-data/wp-content/themes/infobasic/functions.php
+++ wp-data/wp-content/themes/infobasic/functions.php
@@ -11,3 +11,4 @@ function infobasic_scripts(){
     wp_enqueue_script('bootstrap_js', get_theme_file_uri('/assets/js/bootstrap.bundle.min.js'), array(), '4.5', true);
     wp_enqueue_style('bootstrap_css', get_theme_file_uri('/assets/css/bootstrap.min.css'), array(), '4.5', 'all');
 }
+add_action('wp_enqueue_scripts', 'infobasic_scripts');

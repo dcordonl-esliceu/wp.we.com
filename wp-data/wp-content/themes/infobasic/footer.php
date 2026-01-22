167f581da107631f5a5952c0f3d4249814f6f98b
daniel <dcordonl@esliceu.net>
2026-01-22 16:47:06 +0100

    footer

--- wp-data/wp-content/themes/infobasic/footer.php
+++ wp-data/wp-content/themes/infobasic/footer.php
@@ -1,12 +1,10 @@
-<?php
+<footer>
+    <h3>El pie de página</h3>
+</footer>
 
-?>
-    <footer class="site-footer">
-        <div class="container">
-            <p>&copy; <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>. Todos los derechos reservados.</p>
-        </div>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 </body>
-</html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoA6V+7HAuoJl+0I4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SXW5IuS2Zl6TPs+G6X9gGi/YixvKyZ24qMP/9E=" crossorigin="anonymous"></script>
+</head>

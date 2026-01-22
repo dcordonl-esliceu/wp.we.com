17575a32bf84e1c3ac5b8e38e8f724a00e4a9b19
daniel <dcordonl@esliceu.net>
2026-01-22 17:39:10 +0100

    bootstrap actualizado

--- wp-data/wp-content/themes/infobasic/header.php
+++ wp-data/wp-content/themes/infobasic/header.php
@@ -7,4 +7,26 @@
     <title>MotosserasWeb</title>
 </head>
 <body <?php body_class(); ?>>
-<h1>Menú</h1>
+<div class="cabecera">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">Home</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'infobasic' ); ?>">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="<?php echo esc_url( home_url( '/blog' ) ); ?>">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo esc_url( home_url( '/acerca-de' ) ); ?>">Acerca de</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo esc_url( home_url( '/contacto' ) ); ?>">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
